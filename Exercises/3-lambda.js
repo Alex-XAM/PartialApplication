a7375ea0ef2bd6991803b42843ff6e359a5dc483
root <root@HOME-PC.localdomain>
2021-04-01 20:23:51 +0300

    Labs 6 (1, 2, 3)

--- Exercises/3-lambda.js
+++ Exercises/3-lambda.js
@@ -2,10 +2,8 @@
 
 const tagged = (pref, str) => `[${pref}] ${str}`;
 
-// Define function tagDate that prepends current date to the string.
-// E.g. tagDate('My String') === '[2019-11-14] My String'
-// Use function tagged to implement tagDate.
+const currentDate = new Date().toISOString().slice(0, 10);
 
-const tagDate = null;
+const tagDate = str => tagged(currentDate, str);
 
 module.exports = { tagDate };
